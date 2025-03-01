d7e127cc3e703a210c9389d73b18e796616b53e1
David Adams <david@dadams2.com>
2025-03-01 17:04:53 +1100

    attempting to add cool animations. with broken word counter

--- keyboards/crkbd/crkbd.c
+++ keyboards/crkbd/crkbd.c
@@ -18,6 +18,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
+#define ANIM_INVERT false
+#define ANIM_RENDER_WPM true
+#define FAST_TYPE_WPM 45 // Switch to fast animation when over words per minute
+
+#ifdef OLED_ENABLE
+#    include "music-bars.c"
+#endif
+
 #ifdef SWAP_HANDS_ENABLE
 __attribute__((weak)) const keypos_t PROGMEM hand_swap_config[MATRIX_ROWS][MATRIX_COLS] = {
     // Left
@@ -29,8 +37,7 @@ __attribute__((weak)) const keypos_t PROGMEM hand_swap_config[MATRIX_ROWS][MATRI
     {{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}},
     {{0, 1}, {1, 1}, {2, 1}, {3, 1}, {4, 1}, {5, 1}},
     {{0, 2}, {1, 2}, {2, 2}, {3, 2}, {4, 2}, {5, 2}},
-    {{0, 3}, {1, 3}, {2, 3}, {3, 3}, {4, 3}, {5, 3}}
-};
+    {{0, 3}, {1, 3}, {2, 3}, {3, 3}, {4, 3}, {5, 3}}};
 #endif
 
 #ifdef OLED_ENABLE
@@ -150,7 +157,7 @@ bool oled_task_kb(void) {
         oled_render_layer_state();
         oled_render_keylog();
     } else {
-        oled_render_logo();
+        oled_render_anim();
     }
     return false;
 }
--- keyboards/crkbd/rules.mk
+++ keyboards/crkbd/rules.mk
@@ -1,1 +1,3 @@
 DEFAULT_FOLDER = crkbd/rev1
+WPM_ENABLE = yes  # Enable word per minute counter
+RGBLIGHT_ENABLE = no
